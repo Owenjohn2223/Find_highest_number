e7d68cf2f060c654bdc2547ae923becdba847dd9
Owenjohn2223 <owenjohngalamgam@gmail.com>
2024-10-18 01:07:36 +0800

    Done first part - for the inputted numbers

--- find_the_highest_number.py
+++ find_the_highest_number.py
@@ -1,1 +1,8 @@
 # Finding_highest_number
+numbers = input("Enter 5 numbers separated by spaces: ")
+
+num1 = float(numbers[0])
+num2 = float(numbers[1])
+num3 = float(numbers[2])
+num4 = float(numbers[3])
+num5 = float(numbers[4])
