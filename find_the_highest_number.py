2637bc866cc783b515293c93b30700b223fe72c8
Owenjohn2223 <owenjohngalamgam@gmail.com>
2024-10-18 01:21:54 +0800

    Plotted the whole code with algorithm

--- find_the_highest_number.py
+++ find_the_highest_number.py
@@ -6,3 +6,18 @@ num2 = float(numbers[1])
 num3 = float(numbers[2])
 num4 = float(numbers[3])
 num5 = float(numbers[4])
+
+# Assuming na the first number is the highest
+highest = num1
+
+# Compare the first to second number
+if num2 > highest:
+    highest = num2
+
+# ...the third with the fourth number
+
+# ...the fourth with the fith number
+
+# Print off the highest number
+
+
